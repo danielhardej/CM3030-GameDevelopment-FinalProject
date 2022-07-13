8462db6b30f606fe180edcad03557aaf37eeabe9
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2022-07-14 00:11:52 +0100

    Added spawner and script

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -16,13 +16,20 @@ using UnityEngine;
 public class EnemySpawnManager : MonoBehaviour
 {
     [Header("Spawn settings")]
+    [SerializeField, Tooltip("Toggle spawner on/off")]
+    bool active;
     [SerializeField, Tooltip("Time, in seconds, between each spawning")]
     float spawnRate;
-
+    [SerializeField, Tooltip("Maximum number of enemy instances allowed at the same time")]
+    int maxEnemies;
 
     [Header("Spawners")]
-    [SerializeField, Tooltip("Spawner game objects to be managed. These are empty Game Objects denoting the location to spawn")]
-    List<GameObject> spawners;
+    [SerializeField, Tooltip("Spawner game objects to be managed")]
+    List<GameObject> spawnPoints;
+
+    [Header("Enemies")]
+    [SerializeField, Tooltip("Enemy prefabs to spawn")]
+    List<GameObject> enemies;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +40,14 @@ public class EnemySpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    IEnumerator spawn()
+    {
+        // Wait the specified amount of time between spawns
+        yield return new WaitForSeconds(spawnRate);
+
         
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * 
+ * Code by: 
+ *      Dimitrios Vlachos
+ *      djv1@student.london.ac.uk
+ *      dimitri.j.vlachos@gmail.com
+ *      
+ * Adapted from our FSM lecture
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
