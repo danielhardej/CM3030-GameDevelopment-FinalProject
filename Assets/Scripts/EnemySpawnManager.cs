4f7a17b4de92a44adf0fbcb43e8ec1670ea203e1
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2022-07-14 16:01:29 +0100

    Added spawning logic

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -15,39 +15,133 @@ using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
 {
+    #region Variables
     [Header("Spawn settings")]
-    [SerializeField, Tooltip("Toggle spawner on/off")]
-    bool active;
     [SerializeField, Tooltip("Time, in seconds, between each spawning")]
-    float spawnRate;
+    float spawnDelay;
     [SerializeField, Tooltip("Maximum number of enemy instances allowed at the same time")]
     int maxEnemies;
+    [SerializeField, Tooltip("How should enemies be spawned?")]
+    SpawnType spawnType;
 
     [Header("Spawners")]
     [SerializeField, Tooltip("Spawner game objects to be managed")]
     List<GameObject> spawnPoints;
 
     [Header("Enemies")]
-    [SerializeField, Tooltip("Enemy prefabs to spawn")]
-    List<GameObject> enemies;
+    // I left this in with the idea of adding more enemy types in the future, however,
+    // for the sake of simplicity we will just use one for now
+    //[SerializeField, Tooltip("Enemy prefabs to spawn")]
+    //List<GameObject> enemies;
+    [SerializeField, Tooltip("Enemy prefab to spawn")]
+    GameObject enemy;
+    [ReadOnly, Tooltip("List of enemies spawned by this spawner")]
+    List<GameObject> spawnedEntities;
+
+    // This private variable is only used in the distributed spawn type to keep track of the current spawner to use
+    private int spawnerSequence;
+    #endregion
+
+    /* This enumerator allows us to have an easy to use selection between spawn types.
+     * You will see in the inspector, the serialized enum type above creates a dropdown list!
+     * Below, in our StartSpawning() method, you can see how I use this enum to make the selection. */
+    enum SpawnType
+    {
+        Random,
+        Distributed
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Instatiate our emtpy list of entities
+        spawnedEntities = new List<GameObject>();
+
+        // Begin the sequence at the first spawner
+        spawnerSequence = 0;
+
+        // Begin spawning
+        StartCoroutine(StartSpawning());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+         * Every frame we will check through all the entities we have spawned to
+         * make sure they are still alive. If they have been destroyed by the player,
+         * we can remove them from the list, allowing us to spawn more.
+         */
+        foreach (GameObject entity in spawnedEntities)
+        {
+            // The entity object should be null after it is destroyed.
+            if (entity == null)
+            {
+                // Remove this null object fro our list
+                spawnedEntities.Remove(entity);
+            }
+        }
     }
 
-    IEnumerator spawn()
+    IEnumerator StartSpawning()
     {
         // Wait the specified amount of time between spawns
-        yield return new WaitForSeconds(spawnRate);
+        yield return new WaitForSeconds(spawnDelay);
 
-        
+        // If we have reached our enemy cap, don't do anything
+        // We do it in this order for efficiency
+        if (spawnedEntities.Count >= maxEnemies)
+        {
+            Debug.Log("Cannot spawn more enemies, cap reached");
+        }
+        else
+        {
+            if (spawnType == SpawnType.Random)
+            {
+                // Pick a random spawn point from our list of spawners
+                int randomIndex = Random.Range(0, spawnPoints.Count);
+
+                // Spawn enemy
+                SpawnEntity(enemy, spawnPoints[randomIndex].transform.position, spawnPoints[randomIndex].transform.rotation);
+            }
+            else if (spawnType == SpawnType.Distributed)
+            {
+                // Spawn enemy at the current spawner in sequence
+                SpawnEntity(enemy, spawnPoints[spawnerSequence].transform.position, spawnPoints[spawnerSequence].transform.rotation);
+
+                // Increment to the next spawner
+                spawnerSequence++;
+
+                // Ensure we don't overflow the number of spawners.
+                // Modding the step we are on by our maximum number of spawners will
+                // Always wrap around to the correct range of values
+                spawnerSequence %= spawnPoints.Count;
+            }
+        }
+
+        // Start the loop again
+        StartCoroutine(StartSpawning());
+    }
+
+    /// <summary>
+    /// This method spawns the <c>entity</c> given at, <c>position</c>, with <c>rotation</c>.
+    /// It keeps track of the entities spawned autmatically.
+    /// <example>
+    /// For example:
+    /// <code>
+    /// SpawnEntity(enemy, transform.position, transform.rotation);
+    /// </code>
+    /// results in an instance of <c>enemy</c> instantiating at the parent objects position and rotation.
+    /// </example>
+    /// </summary>
+    void SpawnEntity(GameObject entity, Vector3 position, Quaternion rotation)
+    {
+        // Instatiate the enemy entity at the location of the selected spawner.
+        // Save that entity.
+        GameObject spawnedEntity = Instantiate(entity, position, rotation);
+
+        // Add the newly spawned entity to our list of spawned entities,
+        // so we can keep track of the number that have spawned.
+        spawnedEntities.Add(spawnedEntity);
     }
 }
