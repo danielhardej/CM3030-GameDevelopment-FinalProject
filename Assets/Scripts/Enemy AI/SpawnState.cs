3d3194c7c3daa5c66c8bc0f2f3bd56f8c8542470
Ivan Rudolph Whitlock Kruger <haythamnikolaidis@gmail.com>
2022-08-08 19:20:20 +0200

    Added initial enemy animations

---
+++ Assets/Scripts/Enemy AI/SpawnState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnState : BaseState
+{
+
+    public override void EnterState(EnemyFSM npc)
+    {
+        base.EnterState(npc);
+
+        FSM.StartCoroutine(CheckIsOnGround());
+    }
+
+    IEnumerator CheckIsOnGround()
+    {
+        while (!FSM.isOnGround)
+        {
+            yield return null;
+        }
+
+        Debug.Log("Moving from spawn state");
+        animator.SetBool("IsSleeping", false);
+        yield return new WaitForSeconds(1f);
+        FSM.MoveToState(FSM.seek);
+
+    }
+
+}
