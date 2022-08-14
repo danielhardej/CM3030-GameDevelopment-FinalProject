76af8b4edbc491a39656cc1775a9a482a04f3d0c
Ivan Rudolph Whitlock Kruger <haythamnikolaidis@gmail.com>
2022-08-14 23:39:12 +0200

    Test pause menu with audio effects, menu works fine, Using Time.timeScale also works well but WebGL builds dont support audio effects :( so no fancy audio for us.

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using TMPro;
 
 public class HUDController : MonoBehaviour
 {
-
+    public GameObject pauseMenu;
+    public AudioMixer audioMixer;
     private TextMeshProUGUI scoreLabel;
-
     private int score;
     private int displayedScore;
 
@@ -37,5 +38,66 @@ public class HUDController : MonoBehaviour
     public void SetScore(int value)
     {
         score = value;
+    }
+
+    public void OnPauseGame()
+    {
+        StartCoroutine(SlowDownGame());
+        StartCoroutine(SetLowPassFrquency(100f));
+    }
+
+    public void UnpauseGame()
+    {
+        StartCoroutine(SpeedUpGame());
+        StartCoroutine(SetLowPassFrquency(22000f));
+    }
+
+    private IEnumerator SpeedUpGame()
+    {
+        if(pauseMenu.activeInHierarchy)
+        {
+            pauseMenu?.SetActive(false);
+            for (int i = 0; i < 240; i++)
+            {
+                Time.timeScale += 1/240f;
+                if(Time.timeScale >= 1)
+                {
+                    Time.timeScale = 1;
+                    break;
+                }
+                yield return null;
+            }
+        }
+    }
+
+    private IEnumerator SlowDownGame()
+    {
+        if(!pauseMenu.activeInHierarchy)
+        {
+            for (int i = 0; i < 240; i++)
+            {
+                Time.timeScale -= 1/240f;
+                if(Time.timeScale <= 0)
+                {
+                    Time.timeScale = 0;
+                    break;
+                }
+                yield return null;
+            }
+            pauseMenu?.SetActive(true);
+        }
+    }
+
+    private IEnumerator SetLowPassFrquency(float freq)
+    {
+        audioMixer.GetFloat("BG-LowpassFreq", out var currentFreq);
+        var step = (currentFreq - freq)/240;
+
+        for (int i = 0; i < 240; i++)
+        {
+            currentFreq -= step;
+            audioMixer.SetFloat("BG-LowpassFreq", currentFreq);
+            yield return null;
+        }
     }
 }
