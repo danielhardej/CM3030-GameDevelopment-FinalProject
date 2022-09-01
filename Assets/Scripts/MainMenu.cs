31fce09c9cf4414d4ca64217613dbd2e3e812f6c
s c <safa.chawich@gmail.com>
2022-09-01 21:46:48 +0400

    update load screen into tutorial

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,23 @@
-﻿using UnityEngine;
+﻿/*
+* Written by Emilia Hardej and Safa Chawich.
+* Following this tutorial https://youtu.be/YMj2qPq9CP8
+* This class manages the behaviour of MainMenu, Credits, and Loading screen.
+*/
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("MainScene");
-    }
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI text;
+
+    //array to hold Quest1, Quest2, Quest3, Quest4
+    public GameObject[] tutorialState;
 
     public void DisplayCredits()
     {
@@ -17,4 +28,46 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void OnLoadLevel()
+    {
+        // called by the Start button
+        StartCoroutine(BeginTutorial());
+    }
+
+    private IEnumerator BeginTutorial()
+    {
+        loadingScreen.SetActive(true);
+
+        float progress = 0;
+        
+        while (progress != 100)
+        {
+            slider.value = progress;
+            text.SetText($"{progress * 100f + "%"}");
+            yield return null;
+        }
+
+        
+    }
+
+    public void SelectTutorialState()
+    {
+        // loop over the tutorial states and activate each one in turn?
+        // not sure where to call this
+        for (int i = 0; i < tutorialState.Length; i++)
+        tutorialState[i].SetActive(true);
+    }
+
+    private IEnumerator StartingGame()
+    {
+        //load scene after the tutorial is completed
+        var op = SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+
+        while (!op.isDone)
+        {
+            yield return null;
+        }
+    }
+
 }
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,82 @@
+﻿/*
+* Written by Ivan Kruger.
+* This class manages the behaviour of the mini-quests on the Tutorial screen.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using System.Linq;
+
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _pressed, _default;
+
+    public bool isActive = false;
+    public bool isComplete = false;
+
+    void Start()
+    {
+        InputSystem.onEvent.ForDevice<Keyboard>()
+            .Where(e => e.type == new FourCC('S', 'T', 'A', 'T'))
+            .Call(act =>
+            {
+                var button = act.GetFirstButtonPressOrNull();
+
+                if (button != null)
+                {
+                    if (button.displayName == "Up" || button.displayName == "W")
+                    {
+                        Debug.Log("Up Pressed");
+                    }
+
+                    if (button.displayName == "Down" || button.displayName == "S")
+                    {
+                        Debug.Log("Down Pressed");
+                    }
+
+                    if (button.displayName == "Left" || button.displayName == "A")
+                    {
+                        Debug.Log("Left Pressed");
+                    }
+
+                    if (button.displayName == "Right" || button.displayName == "D")
+                    {
+                        Debug.Log("Right Pressed");
+                    }
+
+                    if (button.displayName == "Space")
+                    {
+                        Debug.Log("Space Pressed");
+                    }
+
+                    if (button.displayName == "Esc")
+                    {
+                        Debug.Log("Esc Pressed");
+                    }
+                }
+            });
+
+        InputSystem.onEvent.ForDevice<Mouse>()
+            .Where(e => e.type == new FourCC('S', 'T', 'A', 'T'))
+            .Call(action =>
+            {
+                var button = action.GetFirstButtonPressOrNull();
+
+                if (button != null)
+                {
+                    if (button.displayName == "Left Button")
+                    {
+                        Debug.Log("Mouse Clicked");
+                    }
+                }
+
+            });
+    }
+
+}
